ff3619607ad863b6906749a8116f675b2bdeabc4
KiSungKim23 <essuttr3@gmail.com>
2022-02-07 08:38:10 +0900

    별거 없다. 내림차순 영어로 Descending 기억해  두자
    그림고 greater<자료형>() 요건 기억해 둬야 할듯

--- ForCodingTest/소스.cpp
+++ ForCodingTest/소스.cpp
@@ -5,46 +5,29 @@
 #include <map>
 #include<algorithm>
 
-using namespace std;
-long long solution(int a, int b) {
-    long long answer = 0;
+using namespace std; 
 
-    int iMax = 0;
-    int iMin = 0;
 
-    if (a < b) {
-        iMax = b;
-        iMin = a;
-    }
-    else {
-        iMax = a;
-        iMin = b;
-    }
 
-    for (int i = iMin; i < iMax + 1; i++) {
-        answer += i;
-    }
-
+string solution(string s) {
+    string answer = s;
+    sort(answer.begin(), answer.end(), greater<char>());
     return answer;
 }
+
 void main() {
-    cout << solution(5, 3);
+    cout << solution("Zbcdefg") << endl;
 }
 
 /*
-* 두 정수 a, b가 주어졌을 때 a와 b 사이에 속한 모든 정수의 합을 리턴하는 함수, solution을 완성하세요.
-예를 들어 a = 3, b = 5인 경우, 3 + 4 + 5 = 12이므로 12를 리턴합니다.
+*문제 설명
+문자열 s에 나타나는 문자를 큰것부터 작은 순으로 정렬해 새로운 문자열을 리턴하는 함수, solution을 완성해주세요.
+s는 영문 대소문자로만 구성되어 있으며, 대문자는 소문자보다 작은 것으로 간주합니다.
 
-제한 조건
-a와 b가 같은 경우는 둘 중 아무 수나 리턴하세요.
-a와 b는 -10,000,000 이상 10,000,000 이하인 정수입니다.
-a와 b의 대소관계는 정해져있지 않습니다.
+제한 사항
+str은 길이 1 이상인 문자열입니다.
 입출력 예
-a	b	return
-3	5	12
-3	3	3
-5	3	12
-
-
+s	return
+"Zbcdefg"	"gfedcbZ"
 
 */
