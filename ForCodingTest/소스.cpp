a57dcf63a73cf19dde507c188a0d29c712bf22da
KiSungKim23 <essuttr3@gmail.com>
2022-02-07 11:53:40 +0900

    이건 뭐.... 장난치나?

--- ForCodingTest/소스.cpp
+++ ForCodingTest/소스.cpp
@@ -7,42 +7,26 @@
 
 using namespace std; 
 
-vector<int> solution(vector<int> arr) {
-    vector<int>::iterator iterTemp = arr.begin();
-
-    for (auto iter = arr.begin(); iter != arr.end(); iter++) {
-        if (*iterTemp > *iter) iterTemp = iter;
-    }
-
-    iterTemp = arr.erase(iterTemp);
-
-    if (arr.size() == 0) arr.push_back(-1);
-
-    return arr;
+string solution(int num) {
+    string answer = "";
+    num % 2 == 0 ? answer = "Even" : answer = "Odd";;
+    return answer;
 }
-
 void main() {
-    vector<int> arr = { 4,3,2,1 };
-
-    vector<int> answer = solution(arr);
-
-    for (int i = 0; i < answer.size(); i++) {
-        cout << answer[i] << endl;
-    }
+    cout << solution(5) << endl;
 }
 
 
 /*
-정수를 저장한 배열, arr 에서 가장 작은 수를 제거한 배열을 리턴하는 함수, solution을 완성해주세요. 
-단, 리턴하려는 배열이 빈 배열인 경우엔 배열에 -1을 채워 리턴하세요. 예를들어 arr이 [4,3,2,1]인 경우는 [4,3,2]를 리턴 하고, [10]면 [-1]을 리턴 합니다.
+정수 num이 짝수일 경우 "Even"을 반환하고 홀수인 경우 "Odd"를 반환하는 함수, solution을 완성해주세요.
 
 제한 조건
-arr은 길이 1 이상인 배열입니다.
-인덱스 i, j에 대해 i ≠ j이면 arr[i] ≠ arr[j] 입니다.
+num은 int 범위의 정수입니다.
+0은 짝수입니다.
 입출력 예
-arr	return
-[4,3,2,1]	[4,3,2]
-[10]	[-1]
+num	return
+3	"Odd"
+4	"Even"
 
 
 
