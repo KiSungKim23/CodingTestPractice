6a016bb7198a766a3341c592be5955bd699e208c
KiSungKim23 <essuttr3@gmail.com>
2022-02-08 08:34:28 +0900

    별거 없음

--- ForCodingTest/소스.cpp
+++ ForCodingTest/소스.cpp
@@ -7,83 +7,47 @@
 
 using namespace std; 
 
-vector<int> solution(int n, int m) {
-    vector<int> answer;
+int solution(int num) {
+    int answer = 0;
 
-    vector<int> n약수;
-    vector<int> m약수;
-    for (int i = 1; i <= n; i++) {
-        if (n % i == 0) {
-            n약수.push_back(i);
-        }
-    }
-    for (int i = 1; i <= m; i++) {
-        if (m % i == 0) {
-            m약수.push_back(i);
-        }
+    while (answer < 500 && num != 1) {
+        num % 2 == 1 ? num = num * 3 + 1 : num /= 2;
+        answer++;
     }
 
-    int n_for공약수 = n약수.size() - 1;
-    int m_for공약수 = m약수.size() - 1;
-
-    while (n_for공약수 != 0 || m_for공약수 != 0) {
-        if (n약수[n_for공약수] == m약수[m_for공약수]) {
-            answer.push_back(m약수[m_for공약수]);
-            break;
-        }
-        if (n약수[n_for공약수] < m약수[m_for공약수]) {
-            if (m_for공약수 > 0) m_for공약수--;
-        }
-        else {
-            if (n_for공약수 > 0) n_for공약수--;
-        }
-    }
-
-    if (answer.size() == 0) answer.push_back(0);
-        
-
-    int n_for공배수 = n;
-    int m_for공배수 = m;
-    int n_for공배수_count = 1;
-    int m_for공배수_count = 1;
-
-    while (n_for공배수 != m_for공배수) {
-        if (n_for공배수 > m_for공배수) {
-            m_for공배수 = m * ++m_for공배수_count;
-        }
-        else {
-            n_for공배수 = n * ++n_for공배수_count;
-        }
-    }
-
-    answer.push_back(n_for공배수);
+    if (answer >= 500) answer = -1;
 
     return answer;
 }
 
 void main() {
-    cout << solution(3, 12)[0] << endl;
-    cout << solution(3,12)[1] << endl;
+    cout << solution(626331) << endl;
 }
 
 
 /*
-두 수를 입력받아 두 수의 최대공약수와 최소공배수를 반환하는 함수, solution을 완성해 보세요. 배열의 맨 앞에 최대공약수, 그다음 최소공배수를 넣어 반환하면 됩니다. 
-예를 들어 두 수 3, 12의 최대공약수는 3, 최소공배수는 12이므로 solution(3, 12)는 [3, 12]를 반환해야 합니다.
+1937년 Collatz란 사람에 의해 제기된 이 추측은, 주어진 수가 1이 될때까지 다음 작업을 반복하면, 모든 수를 1로 만들 수 있다는 추측입니다. 작업은 다음과 같습니다.
+
+1-1. 입력된 수가 짝수라면 2로 나눕니다.
+1-2. 입력된 수가 홀수라면 3을 곱하고 1을 더합니다.
+2. 결과로 나온 수에 같은 작업을 1이 될 때까지 반복합니다.
+예를 들어, 입력된 수가 6이라면 6→3→10→5→16→8→4→2→1 이 되어 총 8번 만에 1이 됩니다. 위 작업을 몇 번이나 반복해야하는지 반환하는 함수, solution을 완성해 주세요. 단, 작업을 500번을 반복해도 1이 되지 않는다면 –1을 반환해 주세요.
 
 제한 사항
-두 수는 1이상 1000000이하의 자연수입니다.
+입력된 수, num은 1 이상 8000000 미만인 정수입니다.
 입출력 예
-n	m	return
-3	12	[3, 12]
-2	5	[1, 10]
+n	result
+6	8
+16	4
+626331	-1
 입출력 예 설명
 입출력 예 #1
-위의 설명과 같습니다.
+문제의 설명과 같습니다.
 
 입출력 예 #2
-자연수 2와 5의 최대공약수는 1, 최소공배수는 10이므로 [1, 10]을 리턴해야 합니다.
+16 -> 8 -> 4 -> 2 -> 1 이되어 총 4번만에 1이 됩니다.
 
-
+입출력 예 #3
+626331은 500번을 시도해도 1이 되지 못하므로 -1을 리턴해야합니다.
 
 */
