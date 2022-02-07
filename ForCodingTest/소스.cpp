14af1ccdcd26d8ee3bc3b2da5e989788318005b7
KiSungKim23 <essuttr3@gmail.com>
2022-02-07 11:39:07 +0900

    이건 내가 제일 잘한거 같은뎅

--- ForCodingTest/소스.cpp
+++ ForCodingTest/소스.cpp
@@ -8,27 +8,40 @@
 using namespace std; 
 
 long long solution(long long n) {
-    long long answer;
-    string snToString = to_string(n);
+    long long answer = -1;
 
-    sort(snToString.begin(), snToString.end(),greater<char>());
-   
-    answer = stoll(snToString);
+    long long Count = 0;
+
+    while (Count * Count <= n)
+    {
+        if (Count * Count == n) answer = (Count + 1) * (Count + 1);
+        Count++;
+    }
+
     return answer;
 }
+
 void main() {
-    cout << solution(118372) << endl;
+    cout << solution(3) << endl;
 }
 
 
 /*
-함수 solution은 정수 n을 매개변수로 입력받습니다. n의 각 자릿수를 큰것부터 작은 순으로 정렬한 새로운 정수를 리턴해주세요. 예를들어 n이 118372면 873211을 리턴하면 됩니다.
+임의의 양의 정수 n에 대해, n이 어떤 양의 정수 x의 제곱인지 아닌지 판단하려 합니다.
+n이 양의 정수 x의 제곱이라면 x+1의 제곱을 리턴하고, n이 양의 정수 x의 제곱이 아니라면 -1을 리턴하는 함수를 완성하세요.
 
-제한 조건
-n은 1이상 8000000000 이하인 자연수입니다.
+제한 사항
+n은 1이상, 50000000000000 이하인 양의 정수입니다.
 입출력 예
 n	return
-118372	873211
+121	144
+3	-1
+입출력 예 설명
+입출력 예#1
+121은 양의 정수 11의 제곱이므로, (11+1)를 제곱한 144를 리턴합니다.
+
+입출력 예#2
+3은 양의 정수의 제곱이 아니므로, -1을 리턴합니다.
 
 
 
