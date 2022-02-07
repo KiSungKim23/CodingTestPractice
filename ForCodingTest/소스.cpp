745ab6fa2f1906adbe918f44c33535908ef88a41
KiSungKim23 <essuttr3@gmail.com>
2022-02-07 11:13:32 +0900

    했으면 됬지

--- ForCodingTest/소스.cpp
+++ ForCodingTest/소스.cpp
@@ -7,41 +7,50 @@
 
 using namespace std; 
 
-int solution(int n) {
-    int answer = 1 + n;
-
-    for (int i = n / 2; i > 1; i--) {
-        if (n % i == 0) answer += i;
+string solution(string s) {
+    int iStringSiteTemp = 0;
+    int iCharTemp = 0;
+    string::iterator iterTemp = s.begin();
+    for (int i = 0; i < s.size(); i++) {
+        if (s[i] == ' ') iStringSiteTemp = 0;
+        else {
+            if (iStringSiteTemp % 2 == 0) {
+                if ('a' <= s[i] && 'z' >= s[i]) {
+                    iCharTemp = s[i] - 'a' + 'A';
+                    s[i] = iCharTemp;
+                }
+            }
+            else {
+                if ('A' <= s[i] && 'Z' >= s[i]) {
+                    iCharTemp = s[i] - 'A' + 'a';
+                    s[i] = iCharTemp;
+                }
+            }
+            iStringSiteTemp++;
+        }
     }
-
-    if (n == 0 || n == 1) answer = n;
-
-    return answer;
+    return s;
 }
 
-
 void main() {
-    cout << solution(5) << endl;
+    cout << solution("trAAAasdsady   hello world") << endl;
 
 }
 
 
 /*
-정수 n을 입력받아 n의 약수를 모두 더한 값을 리턴하는 함수, solution을 완성해주세요.
+문자열 s는 한 개 이상의 단어로 구성되어 있습니다. 
+각 단어는 하나 이상의 공백문자로 구분되어 있습니다. 각 단어의 짝수번째 알파벳은 대문자로, 홀수번째 알파벳은 소문자로 바꾼 문자열을 리턴하는 함수, solution을 완성하세요.
 
 제한 사항
-n은 0 이상 3000이하인 정수입니다.
+문자열 전체의 짝/홀수 인덱스가 아니라, 단어(공백을 기준)별로 짝/홀수 인덱스를 판단해야합니다.
+첫 번째 글자는 0번째 인덱스로 보아 짝수번째 알파벳으로 처리해야 합니다.
 입출력 예
-n	return
-12	28
-5	6
+s	return
+"try hello world"	"TrY HeLlO WoRlD"
 입출력 예 설명
-입출력 예 #1
-12의 약수는 1, 2, 3, 4, 6, 12입니다. 이를 모두 더하면 28입니다.
-
-입출력 예 #2
-5의 약수는 1, 5입니다. 이를 모두 더하면 6입니다.
-
+"try hello world"는 세 단어 "try", "hello", "world"로 구성되어 있습니다.
+각 단어의 짝수번째 문자를 대문자로, 홀수번째 문자를 소문자로 바꾸면 "TrY", "HeLlO", "WoRlD"입니다. 따라서 "TrY HeLlO WoRlD" 를 리턴합니다.
 
 
 
