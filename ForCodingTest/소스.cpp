f55a7ff5a5fcd7fcfbbf8506bb3e5d1af0d46677
KiSungKim23 <essuttr3@gmail.com>
2022-02-07 10:18:50 +0900

    stoi 부호까지 바꿔 주더라

--- ForCodingTest/소스.cpp
+++ ForCodingTest/소스.cpp
@@ -7,30 +7,39 @@
 
 using namespace std; 
 
-string solution(int n) {
-    string answer = "";
-
-    for (int i = 0; i < n; i++) {
-        i % 2 == 0 ? answer += "수" : answer += "박";
+int solution(string s) {
+    int answer = 0;
+    if (s[0] == '+') {
+        s.erase(s.begin());
+        answer = stoi(s);
+    }
+    else if (s[0] == '-') {
+        s.erase(s.begin());
+        answer = stoi(s) * -1;
+    }
+    else {
+        answer = stoi(s);
     }
     return answer;
 }
 
 
 void main() {
-    cout << solution(6) << endl;
+    cout << solution("-1234") << endl;
 
 }
 
 /*
-길이가 n이고, "수박수박수박수...."와 같은 패턴을 유지하는 문자열을 리턴하는 함수, solution을 완성하세요. 예를들어 n이 4이면 "수박수박"을 리턴하고 3이라면 "수박수"를 리턴하면 됩니다.
+문자열 s를 숫자로 변환한 결과를 반환하는 함수, solution을 완성하세요.
 
 제한 조건
-n은 길이 10,000이하인 자연수입니다.
+s의 길이는 1 이상 5이하입니다.
+s의 맨앞에는 부호(+, -)가 올 수 있습니다.
+s는 부호와 숫자로만 이루어져있습니다.
+s는 "0"으로 시작하지 않습니다.
 입출력 예
-n	return
-3	"수박수"
-4	"수박수박"
+예를들어 str이 "1234"이면 1234를 반환하고, "-1234"이면 -1234를 반환하면 됩니다.
+str은 부호(+,-)와 숫자로만 구성되어 있고, 잘못된 값이 입력되는 경우는 없습니다.
 
 
 
