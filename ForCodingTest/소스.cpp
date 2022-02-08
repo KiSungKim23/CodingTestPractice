5ceac912381163b9ad07f7a03be9c1de5e6bac48
KiSungKim23 <essuttr3@gmail.com>
2022-02-08 09:12:15 +0900

    깔끔 ㅇㅇ

--- ForCodingTest/소스.cpp
+++ ForCodingTest/소스.cpp
@@ -8,19 +8,9 @@
 
 using namespace std; 
 
-vector<vector<int>> solution(vector<vector<int>> arr1, vector<vector<int>> arr2) {
-    vector<vector<int>> answer;
-
-    vector<int> answer_Temp;
-
-    for (int i = 0; i < arr1.size(); i++) {
-        answer_Temp.clear();
-        for (int j = 0; j < arr1[i].size(); j++) {
-            answer_Temp.push_back(arr1[i][j] + arr2[i][j]);
-        }
-        answer.push_back(answer_Temp);
-    }
-
+vector<long long> solution(int x, int n) {
+    vector<long long> answer;
+    for (int i = 1; i <= n; i++) answer.push_back(x * i);
     return answer;
 }
 void main() {
@@ -29,15 +19,16 @@ void main() {
 
 
 /*
-행렬의 덧셈은 행과 열의 크기가 같은 두 행렬의 같은 행, 같은 열의 값을 서로 더한 결과가 됩니다. 2개의 행렬 arr1과 arr2를 입력받아, 행렬 덧셈의 결과를 반환하는 함수, solution을 완성해주세요.
+함수 solution은 정수 x와 자연수 n을 입력 받아, x부터 시작해 x씩 증가하는 숫자를 n개 지니는 리스트를 리턴해야 합니다. 다음 제한 조건을 보고, 조건을 만족하는 함수, solution을 완성해주세요.
 
 제한 조건
-행렬 arr1, arr2의 행과 열의 길이는 500을 넘지 않습니다.
+x는 -10000000 이상, 10000000 이하인 정수입니다.
+n은 1000 이하인 자연수입니다.
 입출력 예
-arr1	arr2	return
-[[1,2],[2,3]]	[[3,4],[5,6]]	[[4,6],[7,9]]
-[[1],[2]]	[[3],[4]]	[[4],[6]]
-
+x	n	answer
+2	5	[2,4,6,8,10]
+4	3	[4,8,12]
+-4	2	[-4, -8]
 
 
 
