440ca7ab1c934e4a7e84dde256bd1708729b2a49
KiSungKim23 <essuttr3@gmail.com>
2022-02-08 09:15:55 +0900

    string 써서 푼거 씬박하네 ㅋㅋㅋ

--- ForCodingTest/소스.cpp
+++ ForCodingTest/소스.cpp
@@ -8,29 +8,35 @@
 
 using namespace std; 
 
-vector<long long> solution(int x, int n) {
-    vector<long long> answer;
-    for (int i = 1; i <= n; i++) answer.push_back(x * i);
-    return answer;
-}
-void main() {
-
+int main(void) {
+    int a;
+    int b;
+    cin >> a >> b;
+    for (int i = 0; i < b; i++) {
+        for (int j = 0; j < a; j++) {
+            cout << "*";
+        }
+        cout << endl;
+    }
+    return 0;
 }
 
 
 /*
-함수 solution은 정수 x와 자연수 n을 입력 받아, x부터 시작해 x씩 증가하는 숫자를 n개 지니는 리스트를 리턴해야 합니다. 다음 제한 조건을 보고, 조건을 만족하는 함수, solution을 완성해주세요.
+이 문제에는 표준 입력으로 두 개의 정수 n과 m이 주어집니다.
+별(*) 문자를 이용해 가로의 길이가 n, 세로의 길이가 m인 직사각형 형태를 출력해보세요.
 
 제한 조건
-x는 -10000000 이상, 10000000 이하인 정수입니다.
-n은 1000 이하인 자연수입니다.
-입출력 예
-x	n	answer
-2	5	[2,4,6,8,10]
-4	3	[4,8,12]
--4	2	[-4, -8]
+n과 m은 각각 1000 이하인 자연수입니다.
+예시
+입력
 
+5 3
+출력
 
+*****
+*****
+*****
 
 
 
