33f0c3b527187e815f7b655e0c5dfb77ad549769
KiSungKim23 <essuttr3@gmail.com>
2022-02-08 10:10:39 +0900

    어우 코드 하드하다;;

--- ForCodingTest/소스.cpp
+++ ForCodingTest/소스.cpp
@@ -7,36 +7,110 @@
 
 
 using namespace std; 
+int solution(string s) {
+    int answer = -1;
 
-int main(void) {
-    int a;
-    int b;
-    cin >> a >> b;
-    for (int i = 0; i < b; i++) {
-        for (int j = 0; j < a; j++) {
-            cout << "*";
+    string sTemp;
+    string sSubTemp1;
+    string sSubTemp2;
+    int iSubCount = 1;
+
+    for (int i = 1; i <= s.size() / 2; i++) {
+        sTemp.clear();
+        sSubTemp1.clear();
+        sSubTemp2.clear();
+        iSubCount = 1;
+        sSubTemp1 = s.substr(0, i);
+        for (int j = 1; j < s.size() / i; j++) {
+            sSubTemp2 = s.substr(j * i, i);
+            if (sSubTemp1 == sSubTemp2) {
+                iSubCount++;
+            }
+            else {
+                if (iSubCount != 1) {
+                    sSubTemp1 = to_string(iSubCount) + sSubTemp1;
+                    iSubCount = 1;
+                }
+                sTemp = sTemp + sSubTemp1;
+                sSubTemp1 = sSubTemp2;
+            }
+
+            if (j == (s.size() / i) - 1) {
+                if (iSubCount != 1) {
+                    sSubTemp1 = to_string(iSubCount) + sSubTemp1;
+                }
+                sTemp = sTemp + sSubTemp1;
+            }
         }
-        cout << endl;
+        sTemp = sTemp + s.substr((int)(s.size() / i) * i);
+        if (answer < 0) answer = sTemp.size();
+        else if(answer > sTemp.size()) answer = sTemp.size();
     }
+
+    return answer < 0 ? 1 : answer;
+}
+int main(void) {
+
+    cout << solution("aaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaa") << endl;
+    //9
+
     return 0;
 }
 
 
 /*
-이 문제에는 표준 입력으로 두 개의 정수 n과 m이 주어집니다.
-별(*) 문자를 이용해 가로의 길이가 n, 세로의 길이가 m인 직사각형 형태를 출력해보세요.
+데이터 처리 전문가가 되고 싶은 "어피치"는 문자열을 압축하는 방법에 대해 공부를 하고 있습니다.
+최근에 대량의 데이터 처리를 위한 간단한 비손실 압축 방법에 대해 공부를 하고 있는데,
+문자열에서 같은 값이 연속해서 나타나는 것을 그 문자의 개수와 반복되는 값으로 표현하여 더 짧은 문자열로 줄여서 표현하는 알고리즘을 공부하고 있습니다.
+간단한 예로 "aabbaccc"의 경우 "2a2ba3c"(문자가 반복되지 않아 한번만 나타난 경우 1은 생략함)와 같이 표현할 수 있는데,
+이러한 방식은 반복되는 문자가 적은 경우 압축률이 낮다는 단점이 있습니다. 예를 들면, "abcabcdede"와 같은 문자열은 전혀 압축되지 않습니다. 
+"어피치"는 이러한 단점을 해결하기 위해 문자열을 1개 이상의 단위로 잘라서 압축하여 더 짧은 문자열로 표현할 수 있는지 방법을 찾아보려고 합니다.
 
-제한 조건
-n과 m은 각각 1000 이하인 자연수입니다.
-예시
-입력
+예를 들어, "ababcdcdababcdcd"의 경우 문자를 1개 단위로 자르면 전혀 압축되지 않지만, 2개 단위로 잘라서 압축한다면 "2ab2cd2ab2cd"로 표현할 수 있습니다.
+다른 방법으로 8개 단위로 잘라서 압축한다면 "2ababcdcd"로 표현할 수 있으며, 이때가 가장 짧게 압축하여 표현할 수 있는 방법입니다.
 
-5 3
-출력
+다른 예로, "abcabcdede"와 같은 경우, 문자를 2개 단위로 잘라서 압축하면 "abcabc2de"가 되지만, 3개 단위로 자른다면 "2abcdede"가 되어 3개 단위가 가장 짧은 압축 방법이 됩니다.
+이때 3개 단위로 자르고 마지막에 남는 문자열은 그대로 붙여주면 됩니다.
 
-*****
-*****
-*****
+압축할 문자열 s가 매개변수로 주어질 때, 위에 설명한 방법으로 1개 이상 단위로 문자열을 잘라 압축하여 표현한 문자열 중 가장 짧은 것의 길이를 return 하도록 solution 함수를 완성해주세요.
+
+제한사항
+s의 길이는 1 이상 1,000 이하입니다.
+s는 알파벳 소문자로만 이루어져 있습니다.
+입출력 예
+s	result
+"aabbaccc"	7
+"ababcdcdababcdcd"	9
+"abcabcdede"	8
+"abcabcabcabcdededededede"	14
+"xababcdcdababcdcd"	17
+입출력 예에 대한 설명
+입출력 예 #1
+
+문자열을 1개 단위로 잘라 압축했을 때 가장 짧습니다.
+
+입출력 예 #2
+
+문자열을 8개 단위로 잘라 압축했을 때 가장 짧습니다.
+
+입출력 예 #3
+
+문자열을 3개 단위로 잘라 압축했을 때 가장 짧습니다.
+
+입출력 예 #4
+
+문자열을 2개 단위로 자르면 "abcabcabcabc6de" 가 됩니다.
+문자열을 3개 단위로 자르면 "4abcdededededede" 가 됩니다.
+문자열을 4개 단위로 자르면 "abcabcabcabc3dede" 가 됩니다.
+문자열을 6개 단위로 자를 경우 "2abcabc2dedede"가 되며, 이때의 길이가 14로 가장 짧습니다.
+
+입출력 예 #5
+
+문자열은 제일 앞부터 정해진 길이만큼 잘라야 합니다.
+따라서 주어진 문자열을 x / ababcdcd / ababcdcd 로 자르는 것은 불가능 합니다.
+이 경우 어떻게 문자열을 잘라도 압축되지 않으므로 가장 짧은 길이는 17이 됩니다.
+
+
 
 
 
