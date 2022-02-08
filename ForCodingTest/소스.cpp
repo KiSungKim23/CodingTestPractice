6f4be080d9eb739a77630da5a21a0c764b511fe9
KiSungKim23 <essuttr3@gmail.com>
2022-02-08 11:20:10 +0900

    이거 너무 오래걸렸다...

--- ForCodingTest/소스.cpp
+++ ForCodingTest/소스.cpp
@@ -3,113 +3,158 @@
 #include <string>
 #include <vector>
 #include <map>
+#include <sstream>
 #include<algorithm>
 
 
 using namespace std; 
-int solution(string s) {
-    int answer = -1;
 
-    string sTemp;
-    string sSubTemp1;
-    string sSubTemp2;
-    int iSubCount = 1;
+struct UserInfo
+{
+    string UserName;
+    bool Inout;
+};
 
-    for (int i = 1; i <= s.size() / 2; i++) {
-        sTemp.clear();
-        sSubTemp1.clear();
-        sSubTemp2.clear();
-        iSubCount = 1;
-        sSubTemp1 = s.substr(0, i);
-        for (int j = 1; j < s.size() / i; j++) {
-            sSubTemp2 = s.substr(j * i, i);
-            if (sSubTemp1 == sSubTemp2) {
-                iSubCount++;
+
+vector<string> solution(vector<string> record) {
+    vector<string> answer;
+    string answerTemp;
+
+    map<string, string> UserName;
+    map<string, string>::iterator Useriter;
+
+    vector<UserInfo> RecordInfo;
+    UserInfo RecordInfoTemp;
+
+
+    for (int i = 0; i < record.size(); i++) {
+        stringstream Temp;
+        string InoutTemp;
+        string UserIDTemp;
+        string UserNickNameTemp;
+        Temp.str(record[i]);
+        Temp >> InoutTemp;
+        switch (InoutTemp[0]) {
+        case 'E':
+        case 'e':
+            Temp >> UserIDTemp;
+            Temp >> UserNickNameTemp; 
+            Useriter = UserName.find(UserIDTemp);
+            if (Useriter == UserName.end()) {
+                UserName.emplace(pair<string, string>(UserIDTemp, UserNickNameTemp));
             }
             else {
-                if (iSubCount != 1) {
-                    sSubTemp1 = to_string(iSubCount) + sSubTemp1;
-                    iSubCount = 1;
-                }
-                sTemp = sTemp + sSubTemp1;
-                sSubTemp1 = sSubTemp2;
+                Useriter->second = UserNickNameTemp;
             }
-
-            if (j == (s.size() / i) - 1) {
-                if (iSubCount != 1) {
-                    sSubTemp1 = to_string(iSubCount) + sSubTemp1;
-                }
-                sTemp = sTemp + sSubTemp1;
+            RecordInfoTemp.UserName = UserIDTemp;
+            RecordInfoTemp.Inout = true;
+            RecordInfo.push_back(RecordInfoTemp);
+            break;
+        case 'L':
+        case 'l':
+            Temp >> UserIDTemp;
+            RecordInfoTemp.UserName = UserIDTemp;
+            RecordInfoTemp.Inout = false;
+            RecordInfo.push_back(RecordInfoTemp);
+            break;
+        case 'C':;
+        case 'c':
+            Temp >> UserIDTemp;
+            Temp >> UserNickNameTemp; 
+            Useriter = UserName.find(UserIDTemp);
+            if (Useriter == UserName.end()) {
+                UserName.emplace(pair<string, string>(UserIDTemp, UserNickNameTemp));
             }
+            else {
+                Useriter->second = UserNickNameTemp;
+            }
+            break;
         }
-        sTemp = sTemp + s.substr((int)(s.size() / i) * i);
-        if (answer < 0) answer = sTemp.size();
-        else if(answer > sTemp.size()) answer = sTemp.size();
     }
 
-    return answer < 0 ? 1 : answer;
+    for (int i = 0; i < RecordInfo.size(); i++) {
+        if (RecordInfo[i].Inout) {
+            answer.push_back((UserName.find(RecordInfo[i].UserName))->second + "님이 들어왔습니다.");
+        }
+        else {
+            answer.push_back((UserName.find(RecordInfo[i].UserName))->second + "님이 님이 나갔습니다..");
+        }
+    }
+
+    return answer;
 }
+
 int main(void) {
+    vector<string> q = { "Enter uid1234 Muzi", "Enter uid4567 Prodo","Leave uid1234","Enter uid1234 Prodo","Change uid4567 Ryan" };
 
-    cout << solution("aaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaa") << endl;
-    //9
-
+    vector<string> answer = solution(q);
     return 0;
 }
 
 
 /*
-데이터 처리 전문가가 되고 싶은 "어피치"는 문자열을 압축하는 방법에 대해 공부를 하고 있습니다.
-최근에 대량의 데이터 처리를 위한 간단한 비손실 압축 방법에 대해 공부를 하고 있는데,
-문자열에서 같은 값이 연속해서 나타나는 것을 그 문자의 개수와 반복되는 값으로 표현하여 더 짧은 문자열로 줄여서 표현하는 알고리즘을 공부하고 있습니다.
-간단한 예로 "aabbaccc"의 경우 "2a2ba3c"(문자가 반복되지 않아 한번만 나타난 경우 1은 생략함)와 같이 표현할 수 있는데,
-이러한 방식은 반복되는 문자가 적은 경우 압축률이 낮다는 단점이 있습니다. 예를 들면, "abcabcdede"와 같은 문자열은 전혀 압축되지 않습니다. 
-"어피치"는 이러한 단점을 해결하기 위해 문자열을 1개 이상의 단위로 잘라서 압축하여 더 짧은 문자열로 표현할 수 있는지 방법을 찾아보려고 합니다.
+카카오톡 오픈채팅방에서는 친구가 아닌 사람들과 대화를 할 수 있는데, 본래 닉네임이 아닌 가상의 닉네임을 사용하여 채팅방에 들어갈 수 있다.
 
-예를 들어, "ababcdcdababcdcd"의 경우 문자를 1개 단위로 자르면 전혀 압축되지 않지만, 2개 단위로 잘라서 압축한다면 "2ab2cd2ab2cd"로 표현할 수 있습니다.
-다른 방법으로 8개 단위로 잘라서 압축한다면 "2ababcdcd"로 표현할 수 있으며, 이때가 가장 짧게 압축하여 표현할 수 있는 방법입니다.
+신입사원인 김크루는 카카오톡 오픈 채팅방을 개설한 사람을 위해, 다양한 사람들이 들어오고, 나가는 것을 지켜볼 수 있는 관리자창을 만들기로 했다. 채팅방에 누군가 들어오면 다음 메시지가 출력된다.
 
-다른 예로, "abcabcdede"와 같은 경우, 문자를 2개 단위로 잘라서 압축하면 "abcabc2de"가 되지만, 3개 단위로 자른다면 "2abcdede"가 되어 3개 단위가 가장 짧은 압축 방법이 됩니다.
-이때 3개 단위로 자르고 마지막에 남는 문자열은 그대로 붙여주면 됩니다.
+"[닉네임]님이 들어왔습니다."
 
-압축할 문자열 s가 매개변수로 주어질 때, 위에 설명한 방법으로 1개 이상 단위로 문자열을 잘라 압축하여 표현한 문자열 중 가장 짧은 것의 길이를 return 하도록 solution 함수를 완성해주세요.
+채팅방에서 누군가 나가면 다음 메시지가 출력된다.
+
+"[닉네임]님이 나갔습니다."
+
+채팅방에서 닉네임을 변경하는 방법은 다음과 같이 두 가지이다.
+
+채팅방을 나간 후, 새로운 닉네임으로 다시 들어간다.
+채팅방에서 닉네임을 변경한다.
+닉네임을 변경할 때는 기존에 채팅방에 출력되어 있던 메시지의 닉네임도 전부 변경된다.
+
+예를 들어, 채팅방에 "Muzi"와 "Prodo"라는 닉네임을 사용하는 사람이 순서대로 들어오면 채팅방에는 다음과 같이 메시지가 출력된다.
+
+"Muzi님이 들어왔습니다."
+"Prodo님이 들어왔습니다."
+
+채팅방에 있던 사람이 나가면 채팅방에는 다음과 같이 메시지가 남는다.
+
+"Muzi님이 들어왔습니다."
+"Prodo님이 들어왔습니다."
+"Muzi님이 나갔습니다."
+
+Muzi가 나간후 다시 들어올 때, Prodo 라는 닉네임으로 들어올 경우 기존에 채팅방에 남아있던 Muzi도 Prodo로 다음과 같이 변경된다.
+
+"Prodo님이 들어왔습니다."
+"Prodo님이 들어왔습니다."
+"Prodo님이 나갔습니다."
+"Prodo님이 들어왔습니다."
+
+채팅방은 중복 닉네임을 허용하기 때문에, 현재 채팅방에는 Prodo라는 닉네임을 사용하는 사람이 두 명이 있다. 이제, 채팅방에 두 번째로 들어왔던 Prodo가 Ryan으로 닉네임을 변경하면 채팅방 메시지는 다음과 같이 변경된다.
+
+"Prodo님이 들어왔습니다."
+"Ryan님이 들어왔습니다."
+"Prodo님이 나갔습니다."
+"Prodo님이 들어왔습니다."
+
+채팅방에 들어오고 나가거나, 닉네임을 변경한 기록이 담긴 문자열 배열 record가 매개변수로 주어질 때, 모든 기록이 처리된 후, 최종적으로 방을 개설한 사람이 보게 되는 메시지를 문자열 배열 형태로 return 하도록 solution 함수를 완성하라.
 
 제한사항
-s의 길이는 1 이상 1,000 이하입니다.
-s는 알파벳 소문자로만 이루어져 있습니다.
+record는 다음과 같은 문자열이 담긴 배열이며, 길이는 1 이상 100,000 이하이다.
+다음은 record에 담긴 문자열에 대한 설명이다.
+모든 유저는 [유저 아이디]로 구분한다.
+[유저 아이디] 사용자가 [닉네임]으로 채팅방에 입장 - "Enter [유저 아이디] [닉네임]" (ex. "Enter uid1234 Muzi")
+[유저 아이디] 사용자가 채팅방에서 퇴장 - "Leave [유저 아이디]" (ex. "Leave uid1234")
+[유저 아이디] 사용자가 닉네임을 [닉네임]으로 변경 - "Change [유저 아이디] [닉네임]" (ex. "Change uid1234 Muzi")
+첫 단어는 Enter, Leave, Change 중 하나이다.
+각 단어는 공백으로 구분되어 있으며, 알파벳 대문자, 소문자, 숫자로만 이루어져있다.
+유저 아이디와 닉네임은 알파벳 대문자, 소문자를 구별한다.
+유저 아이디와 닉네임의 길이는 1 이상 10 이하이다.
+채팅방에서 나간 유저가 닉네임을 변경하는 등 잘못 된 입력은 주어지지 않는다.
 입출력 예
-s	result
-"aabbaccc"	7
-"ababcdcdababcdcd"	9
-"abcabcdede"	8
-"abcabcabcabcdededededede"	14
-"xababcdcdababcdcd"	17
-입출력 예에 대한 설명
+record	result
+["Enter uid1234 Muzi", "Enter uid4567 Prodo","Leave uid1234","Enter uid1234 Prodo","Change uid4567 Ryan"]	
+["Prodo님이 들어왔습니다.", "Ryan님이 들어왔습니다.", "Prodo님이 나갔습니다.", "Prodo님이 들어왔습니다."]
+입출력 예 설명
 입출력 예 #1
-
-문자열을 1개 단위로 잘라 압축했을 때 가장 짧습니다.
-
-입출력 예 #2
-
-문자열을 8개 단위로 잘라 압축했을 때 가장 짧습니다.
-
-입출력 예 #3
-
-문자열을 3개 단위로 잘라 압축했을 때 가장 짧습니다.
-
-입출력 예 #4
-
-문자열을 2개 단위로 자르면 "abcabcabcabc6de" 가 됩니다.
-문자열을 3개 단위로 자르면 "4abcdededededede" 가 됩니다.
-문자열을 4개 단위로 자르면 "abcabcabcabc3dede" 가 됩니다.
-문자열을 6개 단위로 자를 경우 "2abcabc2dedede"가 되며, 이때의 길이가 14로 가장 짧습니다.
-
-입출력 예 #5
-
-문자열은 제일 앞부터 정해진 길이만큼 잘라야 합니다.
-따라서 주어진 문자열을 x / ababcdcd / ababcdcd 로 자르는 것은 불가능 합니다.
-이 경우 어떻게 문자열을 잘라도 압축되지 않으므로 가장 짧은 길이는 17이 됩니다.
-
+문제의 설명과 같다.
 
 
 
