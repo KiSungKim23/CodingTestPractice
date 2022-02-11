52cde72f5bb72cfcab05c650596befcf79ef599f
KiSungKim23 <essuttr3@gmail.com>
2022-02-11 09:40:58 +0900

    조큼 쉽게 푼거 같기는 한데

--- ForCodingTest/소스.cpp
+++ ForCodingTest/소스.cpp
@@ -9,78 +9,72 @@
 
 using namespace std; 
 
-void Translation_123Word(vector<int>* Array,int index) {
-    if ((*Array)[index] <= 0) {
-        (*Array)[index] += 3;
-        (*Array)[index + 1] -= 1;
-        if (index + 1 < Array->size() - 1) {
-            Translation_123Word(Array, index + 1);
-        }
-    }
-}
+vector<int> solution(vector<int> progresses, vector<int> speeds) {
+    vector<int> answer;
 
-string solution(int n) {
-    string answer = "";
-    vector<int> vecThreeNotation;
+    int index = 0;
+    int DeploymentCount = 0;
 
-    while (n > 2) {
-        vecThreeNotation.push_back(n % 3);
-        n /= 3;
-    }
-    vecThreeNotation.push_back(n);
-
-    for (int i = vecThreeNotation.size() - 1; i >= 0; i--) {
-        Translation_123Word(&vecThreeNotation, i);
-    }
-
-    for (int i = vecThreeNotation.size() - 1; i >= 0; i--) {
-        switch (vecThreeNotation[i])
+    while (index < progresses.size()) {
+        while (progresses[index] < 100)
         {
-        case 1:
-            answer.push_back('1');
-            break;
-        case 2:
-            answer.push_back('2');
-            break;
-        case 3:
-            answer.push_back('4');
-            break;
+            for (int i = index; i < progresses.size(); i++) {
+                progresses[i] += speeds[i];
+            }
         }
+
+        while (index < progresses.size() && progresses[index] >= 100) {
+            index++;
+            DeploymentCount++;
+        }
+
+        answer.push_back(DeploymentCount);
+        DeploymentCount = 0;
     }
+
 
     return answer;
 }
-
 int main(void) {
-    cout << solution(10);
+    vector<int> progresses = { 95, 90, 99, 99, 80, 99 };
+    vector<int> speeds = { 1, 1, 1, 1, 1, 1 };
+
+    solution(progresses, speeds);
     return 0;
 }
 
 
 /*
-124 나라가 있습니다. 124 나라에서는 10진법이 아닌 다음과 같은 자신들만의 규칙으로 수를 표현합니다.
+프로그래머스 팀에서는 기능 개선 작업을 수행 중입니다. 각 기능은 진도가 100%일 때 서비스에 반영할 수 있습니다.
 
-124 나라에는 자연수만 존재합니다.
-124 나라에는 모든 수를 표현할 때 1, 2, 4만 사용합니다.
-예를 들어서 124 나라에서 사용하는 숫자는 다음과 같이 변환됩니다.
+또, 각 기능의 개발속도는 모두 다르기 때문에 뒤에 있는 기능이 앞에 있는 기능보다 먼저 개발될 수 있고, 이때 뒤에 있는 기능은 앞에 있는 기능이 배포될 때 함께 배포됩니다.
 
-10진법	124 나라	10진법	124 나라
-1	1	6	14
-2	2	7	21
-3	4	8	22
-4	11	9	24
-5	12	10	41
+먼저 배포되어야 하는 순서대로 작업의 진도가 적힌 정수 배열 progresses와 각 작업의 개발 속도가 적힌 정수 배열 speeds가 주어질 때 
+각 배포마다 몇 개의 기능이 배포되는지를 return 하도록 solution 함수를 완성하세요.
 
-자연수 n이 매개변수로 주어질 때, n을 124 나라에서 사용하는 숫자로 바꾼 값을 return 하도록 solution 함수를 완성해 주세요.
-
-제한사항
-n은 500,000,000이하의 자연수 입니다.
+제한 사항
+작업의 개수(progresses, speeds배열의 길이)는 100개 이하입니다.
+작업 진도는 100 미만의 자연수입니다.
+작업 속도는 100 이하의 자연수입니다.
+배포는 하루에 한 번만 할 수 있으며, 하루의 끝에 이루어진다고 가정합니다. 예를 들어 진도율이 95%인 작업의 개발 속도가 하루에 4%라면 배포는 2일 뒤에 이루어집니다.
 입출력 예
-n	result
-1	1
-2	2
-3	4
-4	11
+progresses	speeds	return
+[93, 30, 55]	[1, 30, 5]	[2, 1]
+[95, 90, 99, 99, 80, 99]	[1, 1, 1, 1, 1, 1]	[1, 3, 2]
+입출력 예 설명
+입출력 예 #1
+첫 번째 기능은 93% 완료되어 있고 하루에 1%씩 작업이 가능하므로 7일간 작업 후 배포가 가능합니다.
+두 번째 기능은 30%가 완료되어 있고 하루에 30%씩 작업이 가능하므로 3일간 작업 후 배포가 가능합니다. 하지만 이전 첫 번째 기능이 아직 완성된 상태가 아니기 때문에 첫 번째 기능이 배포되는 7일째 배포됩니다.
+세 번째 기능은 55%가 완료되어 있고 하루에 5%씩 작업이 가능하므로 9일간 작업 후 배포가 가능합니다.
+
+따라서 7일째에 2개의 기능, 9일째에 1개의 기능이 배포됩니다.
+
+입출력 예 #2
+모든 기능이 하루에 1%씩 작업이 가능하므로, 작업이 끝나기까지 남은 일수는 각각 5일, 10일, 1일, 1일, 20일, 1일입니다. 어떤 기능이 먼저 완성되었더라도 앞에 있는 모든 기능이 완성되지 않으면 배포가 불가능합니다.
+
+따라서 5일째에 1개의 기능, 10일째에 3개의 기능, 20일째에 2개의 기능이 배포됩니다.
+
+※ 공지 - 2020년 7월 14일 테스트케이스가 추가되었습니다.
 
 
 
