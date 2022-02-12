6e57668fad22d6d4488e24eda172eadb78f5c3b7
KiSungKim23 <essuttr3@gmail.com>
2022-02-12 11:47:05 +0900

    도저히 효율성 검사를 통과하질 못하네 한번 순회하는데 뭐가 그렇게 맘에 안드는 거니....

--- ForCodingTest/소스.cpp
+++ ForCodingTest/소스.cpp
@@ -10,77 +10,127 @@
 
 using namespace std; 
 
-int solution(vector<int> scoville, int K) {
-    int answer1 = 0;
-    int answer = 0;
-    int needHot;
-    priority_queue<int, vector<int>, greater<int>> pq(scoville.begin(), scoville.end());
 
-    while (pq.top() < K) {
-        if (pq.size() == 1) return answer = -1;
-        needHot = pq.top(); pq.pop();
-        pq.push(needHot + pq.top() * 2);
-        pq.pop(); answer++;
+int solution(string s)
+{
+    int index = 0;
+    string::iterator siter = s.begin();
+
+    while(siter != --(s.end())){
+        if (!((*siter) ^ *(++siter))) {
+            siter = s.erase(siter);
+            siter = s.erase(--siter);
+            if (siter != s.begin()) {
+                siter--;
+            }
+        }
+
+        if (s.size() < 1) {
+            break;
+        }
     }
 
-    return answer;
+    return s.size() == 0 ? 1 : 0;
 }
-
-
-
-
-int solution(vector<int> scoville, int K) {
-    int answer = 0;
-    int needHot;
-    priority_queue<int, vector<int>, greater<int>> pq(scoville.begin(), scoville.end());
-
-    while (pq.top() < K) {
-        if (pq.size() == 1) return answer = -1;
-        needHot = pq.top(); pq.pop();
-        pq.push(needHot + pq.top() * 2);
-        pq.pop(); answer++;
-    }
-
-    return answer;
-}
-
-
 
 int main(void) {
-    vector<int> progresses = { 1, 2,3,15};
 
-    cout << solution(progresses, 15);
+    cout << solution("baabaa");
     return 0;
 }
 
 
 /*
-매운 것을 좋아하는 Leo는 모든 음식의 스코빌 지수를 K 이상으로 만들고 싶습니다. 
-모든 음식의 스코빌 지수를 K 이상으로 만들기 위해 Leo는 스코빌 지수가 가장 낮은 두 개의 음식을 아래와 같이 특별한 방법으로 섞어 새로운 음식을 만듭니다.
+짝지어 제거하기는, 알파벳 소문자로 이루어진 문자열을 가지고 시작합니다.
+먼저 문자열에서 같은 알파벳이 2개 붙어 있는 짝을 찾습니다.
+그다음, 그 둘을 제거한 뒤, 앞뒤로 문자열을 이어 붙입니다. 이 과정을 반복해서 문자열을 모두 제거한다면 짝지어 제거하기가 종료됩니다. 
+문자열 S가 주어졌을 때, 짝지어 제거하기를 성공적으로 수행할 수 있는지 반환하는 함수를 완성해 주세요. 성공적으로 수행할 수 있으면 1을, 아닐 경우 0을 리턴해주면 됩니다.
 
-섞은 음식의 스코빌 지수 = 가장 맵지 않은 음식의 스코빌 지수 + (두 번째로 맵지 않은 음식의 스코빌 지수 * 2)
-Leo는 모든 음식의 스코빌 지수가 K 이상이 될 때까지 반복하여 섞습니다.
-Leo가 가진 음식의 스코빌 지수를 담은 배열 scoville과 원하는 스코빌 지수 K가 주어질 때, 
-모든 음식의 스코빌 지수를 K 이상으로 만들기 위해 섞어야 하는 최소 횟수를 return 하도록 solution 함수를 작성해주세요.
+예를 들어, 문자열 S = baabaa 라면
 
-제한 사항
-scoville의 길이는 2 이상 1,000,000 이하입니다.
-K는 0 이상 1,000,000,000 이하입니다.
-scoville의 원소는 각각 0 이상 1,000,000 이하입니다.
-모든 음식의 스코빌 지수를 K 이상으로 만들 수 없는 경우에는 -1을 return 합니다.
+b aa baa → bb aa → aa →
+
+의 순서로 문자열을 모두 제거할 수 있으므로 1을 반환합니다.
+
+제한사항
+문자열의 길이 : 1,000,000이하의 자연수
+문자열은 모두 소문자로 이루어져 있습니다.
 입출력 예
-scoville	K	return
-[1, 2, 3, 9, 10, 12]	7	2
+s	result
+baabaa	1
+cdcd	0
 입출력 예 설명
-스코빌 지수가 1인 음식과 2인 음식을 섞으면 음식의 스코빌 지수가 아래와 같이 됩니다.
-새로운 음식의 스코빌 지수 = 1 + (2 * 2) = 5
-가진 음식의 스코빌 지수 = [5, 3, 9, 10, 12]
+입출력 예 #1
+위의 예시와 같습니다.
+입출력 예 #2
+문자열이 남아있지만 짝지어 제거할 수 있는 문자열이 더 이상 존재하지 않기 때문에 0을 반환합니다.
 
-스코빌 지수가 3인 음식과 5인 음식을 섞으면 음식의 스코빌 지수가 아래와 같이 됩니다.
-새로운 음식의 스코빌 지수 = 3 + (5 * 2) = 13
-가진 음식의 스코빌 지수 = [13, 9, 10, 12]
+※ 공지 - 2020년 6월 8일 테스트케이스가 추가되었습니다.
 
-모든 음식의 스코빌 지수가 7 이상이 되었고 이때 섞은 횟수는 2회입니다.
+
+int findbehind(bool* arrbool,int behind) {
+    while (!arrbool[behind]) {
+        behind--;
+        if (behind < 0)
+            break;
+    }
+
+    return behind;
+}
+
+void deleteword(string s, bool* arrbool, int iBehind,int* index) {
+    if (iBehind >= 0 && arrbool[iBehind] && s[iBehind] == s[*index + 1]) {
+        arrbool[iBehind] = false;
+        arrbool[++(*index)] = false;
+        iBehind = findbehind(arrbool, iBehind);
+        if (iBehind >= 0) {
+            deleteword(s, arrbool, iBehind, index);
+        }
+    }
+}
+
+
+int solution(string s)
+{
+    bool answer = false;
+    int index = 0;
+    int iBehind = 0;
+    bool* bDeleteCheck = new bool[s.size()];
+
+    while(index < s.size() - 1){
+        bDeleteCheck[index] = true;
+
+        if (s[index] == s[index + 1]) {
+            bDeleteCheck[index] = false;
+            bDeleteCheck[++index] = false;
+        }
+
+        iBehind = index;
+
+        iBehind = findbehind(bDeleteCheck, iBehind);
+
+        deleteword(s, bDeleteCheck, iBehind, &index);
+
+        index++;
+    }
+
+    for (int i = 0; i < s.size(); i++) {
+        if (bDeleteCheck[i]) {
+            answer = true;
+            break;
+        }
+    }
+
+    delete[] bDeleteCheck;
+    return answer ? 0 : 1;
+}
+
+int main(void) {
+
+    cout << solution("aabbbaab");
+    return 0;
+}
+
 
 
 
